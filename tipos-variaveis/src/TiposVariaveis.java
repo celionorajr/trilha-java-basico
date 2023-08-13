c300b8ee77af679c1361675d78ccec84dc178578
Célio Nora <cnoraj@gmail.com>
2023-08-13 14:23:06 -0300

    alteração

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Célio Nora Junior";
+
+        double salario = 2500;
+
+        short numeroCurto1 = 1;
+        int numeroNormal = numeroCurto1;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_PI = 3.14;
+
     }
 }
