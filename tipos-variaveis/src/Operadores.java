304fbbb69707e2ffbae762bf690552fca158e043
Célio Nora <cnoraj@gmail.com>
2023-08-13 23:35:23 -0300

    atualização de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeUm = "Célio";
+        String nomeDois = new String("Célio");
+
+        System.out.println(nomeUm.equals(nomeDois));//O método equals é utilizado para comparações.
+    }
+}
